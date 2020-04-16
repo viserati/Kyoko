c272827936df20e02106ae022abf0256c3f6b089
Keith Turner <turner@keithturner.com>
2020-04-15 22:16:02 -0600

    Templates

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [
+    {
+        'author' : 'Anthony Soprano',
+        'title' : 'Posting 1',
+        'content' : 'First post content ...',
+        'date_posted' : 'April 15, 2020',
+    },
+
+    {
+        'author' : 'Junior Soprano',
+        'title' : 'Posting 2',
+        'content' : 'Second post content ...',
+        'date_posted' : 'April 16, 2020',
+
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-	return '<h1>Home Page</h1>'
+	return render_template('home.html', posts = posts)
 
 @app.route('/about')
 def about():
-	return '<h1>About Page</h1>'
+	return render_template('about.html', title = 'About !!!')
 
 
 # Code below is so you don't have to turn the server on/off after each change.
