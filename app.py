5648e0231eb5997f99dc5aca4cce1d07f24d9150
Keith Turner <turner@keithturner.com>
2020-04-16 23:36:27 -0600

    Forms and User Input [1]

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '137affc4bc7a432606e0532aa2e85310'
 
 posts = [
     {
@@ -27,6 +30,15 @@ def home():
 def about():
 	return render_template('about.html', title = 'About !!!')
 
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title = 'Register', form = form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Login', form = form)
 
 # Code below is so you don't have to turn the server on/off after each change.
 if __name__ == '__main__':
