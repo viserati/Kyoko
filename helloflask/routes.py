108d4ff74a175c8cb424ecf880e01be920e3598c
Keith Turner <turner@keithturner.com>
2020-04-28 09:21:24 -0600

    User Account and Profile Picture 5

--- helloflask/routes.py
+++ helloflask/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from helloflask import app, db, bcrypt
-from helloflask.forms import RegistrationForm, LoginForm
+from helloflask.forms import RegistrationForm, LoginForm,UpdateAccountForm
 from helloflask.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -75,5 +75,6 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
+    form = UpdateAccountForm()
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
