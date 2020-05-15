6ac794de695b8fe147068f2232044d98b0c36086
Keith Turner <turner@keithturner.com>
2020-05-14 20:53:33 -0600

    Email and Password Reset

--- helloflask/routes.py
+++ helloflask/routes.py
@@ -3,7 +3,7 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from helloflask import app, db, bcrypt
-from helloflask.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from helloflask.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, RequestResetForm, ResetPasswordForm
 from helloflask.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -148,3 +148,21 @@ def user_posts(username):
     .order_by(Post.date_posted.desc())\
     .paginate(page=page, per_page=3)
     return render_template('user_posts.html', posts=posts, user=user)
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+        form = RequestResetForm()
+        return render_template('reset_request.html', title ='Reset Password', form=form)
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token
+    if user is None:
+        flash('That is an invalid or expired token', 'warning:')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    return render_template('reset_token.html', title ='Reset Password', form=form)
